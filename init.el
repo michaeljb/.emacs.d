dfdf5750654f90d6d4b4f6adf02bfaf1994c76ed
Michael Brandt <michael.j.brandt@colorado.edu>
2013-07-30 09:10:18 -0600

    update ignore, add floobits, fix some indentation and mode stuff

--- init.el
+++ init.el
@@ -7,14 +7,25 @@
 
 (require 'keyboard)
 
+(add-to-list 'auto-mode-alist (cons (rx ".js" eos) 'js2-mode))
+
+(setq auto-mode-alist  (cons '("Gemfile$" . ruby-mode) auto-mode-alist))
+(setq auto-mode-alist  (cons '("Gemfile.lock$" . ruby-mode) auto-mode-alist))
+(setq auto-mode-alist  (cons '("Rakefile$" . ruby-mode) auto-mode-alist))
+(add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
+
+
 ;; CUSTOM
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (load custom-file)
 
-(add-to-list 'auto-mode-alist (cons (rx ".js" eos) 'js2-mode))
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 2)
+(setq-default js2-basic-offset 2)
 
 ;; (setq filesets-menu-path ("file"))
 ;; (setq filesets-menu-before "Open File...")
 
 (filesets-init) 
 (setq filesets-cache-save-often-flag t)
+(load "~/.emacs.d/floobits/floobits.el")
--- settings.el
+++ settings.el
@@ -13,9 +13,6 @@
 ;; javascript-mode
 (setq js-indent-level 2)
 
-;; (autoload 'js2-mode "js2" nil t)
-;; (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
-
 ;; Edit with Emacs for Chrome
 (require 'edit-server)
 (edit-server-start)
