19202f367f2362731c2b9e6f2e4f62bcf64ad98c
Michael Brandt <mbrandt@colorado.edu>
2014-07-08 16:07:50 -0600

    jshint working in subdirs for the project; needed to set full path

--- projects/nsidc-acadis-search-portal.el
+++ projects/nsidc-acadis-search-portal.el
@@ -3,4 +3,4 @@
 
 ((js2-mode
   (flycheck-javascript-jshint-executable . "~/dev-vm/search/projects/nsidc-acadis-search-portal/node_modules/grunt-contrib-jshint/node_modules/jshint/bin/jshint")
-  (flycheck-jshintrc . "config/.jshintrc")))
+  (flycheck-jshintrc . "~/dev-vm/search/projects/nsidc-acadis-search-portal/config/.jshintrc")))
