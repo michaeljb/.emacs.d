994d9debb34dfdd1e4ff30299bba4bf4586104fb
Michael Brandt <mbrandt@colorado.edu>
2014-07-08 16:52:08 -0600

    remove comment

--- init.el
+++ init.el
@@ -309,7 +309,6 @@
 ;; -------------------------------------
 
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-;;(add-to-list 'ac-modes 'js2-mode)
 
 (add-hook 'js2-mode-hook 'ac-js2-mode)
 (setq ac-js2-evaluate-calls t)
