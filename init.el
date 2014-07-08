5b2a7151f1847f34cc57663a73c00863e870c83d
Michael Brandt <mbrandt@colorado.edu>
2014-07-08 16:51:54 -0600

    fix loading other modes for js files

--- init.el
+++ init.el
@@ -41,6 +41,8 @@
     rainbow-delimiters
     rvm
     smex
+    tern
+    tern-auto-complete
     auto-indent-mode
     git-commit-mode
     git-rebase-mode
@@ -306,14 +308,12 @@
 ;; JavaScript
 ;; -------------------------------------
 
-(add-to-list 'ac-modes 'js2-mode)
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+;;(add-to-list 'ac-modes 'js2-mode)
 
 (add-hook 'js2-mode-hook 'ac-js2-mode)
 (setq ac-js2-evaluate-calls t)
 
-(rename-modeline "js2-mode" js2-mode "js2")
-
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 (setq-default js2-basic-offset 2)
 (setq-default js2-allow-keywords-as-property-names nil)
 
@@ -323,9 +323,9 @@
 (setq-default js2-show-parse-errors nil)
 (setq-default js2-strict-missing-semi-warning nil)
 (setq-default js2-strict-trailing-comma-warning t) ;; jshint does not warn about this now for some reason
+(add-hook 'js2-mode-hook (lambda () (flycheck-mode 1)))
 
-(add-hook 'js2-mode-hook (lambda () (auto-complete-mode t)))
-(add-hook 'js2-mode-hook (lambda () (flycheck-mode t)))
+;; (add-hook 'js2-mode-hook (lambda () (auto-complete-mode)))
 (add-hook 'js2-mode-hook (lambda () (tern-mode t)))
 (eval-after-load 'tern
   '(progn
