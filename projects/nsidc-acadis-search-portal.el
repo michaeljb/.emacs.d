5fe945f37ad91ff451610722481ffa3ecffc0b4b
Michael Brandt <mbrandt@colorado.edu>
2015-12-28 11:05:20 -0700

    nuke it all

--- projects/nsidc-acadis-search-portal.el
+++
@@ -1,5 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((js2-mode
-  (flycheck-javascript-jshint-executable . "~/dev-vm/search/projects/nsidc-acadis-search-portal/node_modules/grunt-contrib-jshint/node_modules/jshint/bin/jshint")))
