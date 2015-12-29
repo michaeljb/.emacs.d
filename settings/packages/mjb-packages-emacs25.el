9a088f5b250904f31e171e548fe915a9b39c1a80
Michael Brandt <mbrandt@colorado.edu>
2015-12-29 11:36:36 -0700

    Define package list in mjb-packages
    When I add support for installing packages in other versions of emacs,
    I'll want to only list the packages to install once.

--- settings/packages/mjb-packages-emacs25.el
+++ settings/packages/mjb-packages-emacs25.el
@@ -1,9 +1,7 @@
 (setq package-enable-at-startup nil)
 (package-initialize)
 
-(setq package-selected-packages '(
-				  magit
-				  ))
+(setq package-selected-packages mjb-package-list)
 
 (package-install-selected-packages)
 (package-autoremove)
--- settings/packages/mjb-packages.el
+++ settings/packages/mjb-packages.el
@@ -1,6 +1,10 @@
 (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
 			 ("melpa" . "https://melpa.org/packages/")))
 
+(setq mjb-package-list '(
+			 magit
+			 ))
+
 (cond (t (require 'mjb-packages-emacs25)))
 
 (provide 'mjb-packages)
