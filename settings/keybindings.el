76cbc21fdfdc03a87e34265b33fdcae9364049c4
Michael Brandt <mbrandt@colorado.edu>
2015-12-28 17:51:48 -0700

    alternate m-x

--- settings/keybindings.el
+++ settings/keybindings.el
@@ -16,4 +16,7 @@
 
 (global-set-key (kbd "A-l") 'linum-mode)
 
+(global-set-key "\C-x\C-m" 'execute-extended-command)
+(global-set-key "\C-c\C-m" 'execute-extended-command)
+
 (provide 'keybindings)
