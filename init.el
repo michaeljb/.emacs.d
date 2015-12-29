b331c70aee0007c9c1f9b5ed5871a5c78f219365
Michael Brandt <mbrandt@colorado.edu>
2015-12-29 12:35:57 -0700

    Add paren-showing stuff

--- init.el
+++ init.el
@@ -29,6 +29,19 @@
 
 (setq shift-select-mode nil)
 
+(show-paren-mode t)
+(setq show-paren-delay 0)
+(defadvice show-paren-function
+      (after show-matching-paren-offscreen activate)
+      "If the matching paren is offscreen, show the matching line in the
+        echo area. Has no effect if the character before point is not of
+        the syntax class ')'."
+      (interactive)
+      (let* ((cb (char-before (point)))
+             (matching-text (and cb
+                                 (char-equal (char-syntax cb) ?\) )
+                                 (blink-matching-open))))
+        (when matching-text (message matching-text))))
 ;; modeline
 
 (setq line-number-mode t)
