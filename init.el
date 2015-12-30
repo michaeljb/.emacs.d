3cec32a4512b0584199c508a34f4edeab991cb5d
Michael Brandt <mbrandt@colorado.edu>
2015-12-29 17:40:45 -0700

    Create dev directory

--- init.el
+++ init.el
@@ -1,13 +1,16 @@
 ;; (package-initialize) ;; added aggressively by package.el, commented here and
-                        ;; handled in mjb-packages-emacs25.el
+                        ;; handled in mjb-packages.el
 
 (setq custom-file (locate-user-emacs-file ".custom.el"))
 (load custom-file t t)
 
 (setq settings-dir (expand-file-name "settings" user-emacs-directory))
+(setq dev-dir (expand-file-name "dev" settings-dir))
 (setq os-dir (expand-file-name "os" settings-dir))
 (setq packages-dir (expand-file-name "packages" settings-dir))
+
 (add-to-list 'load-path settings-dir)
+(add-to-list 'load-path dev-dir)
 (add-to-list 'load-path os-dir)
 (add-to-list 'load-path packages-dir)
 
