310010cb5d265779cfbaca60ac4459d4a95f4dab
Michael Brandt <mbrandt@colorado.edu>
2016-01-08 16:24:54 -0700

    Shorter :bell: message
    Don't take two lines when the window is half the width of my work laptop
    screen.

--- settings/mjb-minibuffer.el
+++ settings/mjb-minibuffer.el
@@ -4,6 +4,6 @@
 
 (setq echo-keystrokes 0.01)
 
-(setq ring-bell-function (lambda () (message "*woop* *woop* *woop* *woop* *woop* *woop* *woop* *woop* *woop* *woop* *woop* *woop* *woop* *woop* *woop* *woop* ")(sleep-for .15)))
+(setq ring-bell-function (lambda () (message "*woop* *woop* *woop* *woop* *woop* *woop* *woop* *woop* *woop* *woop* *woop* *woop* *woop* *woop*")(sleep-for .15)))
 
 (provide 'mjb-minibuffer)
