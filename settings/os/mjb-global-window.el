03c007954c75bbd22c378ae09342e583e1242972
Michael Brandt <mbrandt@colorado.edu>
2016-02-10 15:11:40 -0700

    env-window: load environment path from .bashrc

--- settings/os/mjb-global-window.el
+++ settings/os/mjb-global-window.el
@@ -3,4 +3,12 @@
 (global-set-key (kbd "M-<up>")    'windmove-up)
 (global-set-key (kbd "M-<down>")  'windmove-down)
 
+;; https://gist.github.com/jakemcc/3887459
+(defun set-exec-path-from-shell-PATH ()
+  (setenv "PATH" (concat "/usr/local/bin:" (getenv "PATH")))
+  (let ((path-from-shell (shell-command-to-string "$SHELL -i -c 'echo -n $PATH'")))
+    (setenv "PATH" path-from-shell)
+    (setq exec-path (split-string path-from-shell path-separator))))
+(set-exec-path-from-shell-PATH)
+
 (provide 'mjb-global-window)
