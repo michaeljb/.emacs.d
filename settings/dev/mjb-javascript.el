9613ff7ee74c1bf4ab52a57b86beae78e0aab043
Michael Brandt <michaelbrandt5@gmail.com>
2016-04-15 12:03:19 -0600

    js: don't show strict warnings

--- settings/dev/mjb-javascript.el
+++ settings/dev/mjb-javascript.el
@@ -1,4 +1,6 @@
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.jsx\\'" . js2-jsx-mode))
 
+(setq js2-mode-show-strict-warnings nil)
+
 (provide 'mjb-javascript)
