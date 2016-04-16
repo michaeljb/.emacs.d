3911c99ca9a36926c3c0a5f4098373e1dde8bddb
Michael Brandt <michaelbrandt5@gmail.com>
2016-04-15 21:09:26 -0600

    CSS indentation

--- .custom.el
+++ .custom.el
@@ -4,6 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(column-number-mode t)
+ '(css-indent-offset 2)
  '(fill-column 80)
  '(js-indent-level 2)
  '(js2-basic-offset 2)
