24d0e7d87104a50c36f532701aa99cd7c2a1d426
Michael Brandt <mbrandt@colorado.edu>
2016-07-27 14:23:45 -0600

    go go use-package

--- settings-emacs25/mjb-install-packages.el
+++ settings-emacs25/mjb-install-packages.el
@@ -1,5 +1,4 @@
 (setq package-selected-packages mjb-package-list)
 (package-install-selected-packages)
-(package-autoremove)
 
 (provide 'mjb-install-packages)
--- settings/mjb-packages.el
+++ settings/mjb-packages.el
@@ -7,59 +7,81 @@
 (package-initialize)
 
 (defvar mjb-package-list
-  '(
-    ag
-    counsel
-    csv-mode
-    dash
-    ein
-    expand-region
-    hide-comnt
-    hydra
-    ivy
-    ivy-hydra
-    magit
-    multiple-cursors
-    phi-search
-    projectile
-    rainbow-delimiters
-    smex
-    swiper
-    wc-mode
-    wgrep
-    wgrep-ag
-    zenburn-theme
+  '(use-package))
 
-    ;; docker
-    dockerfile-mode
-
-    ;; gherkin
-    feature-mode
-
-    ;; javascript
-    js2-mode
-
-    ;; json
-    json-mode
-    json-reformat
-    json-snatcher
-
-    ;; markdown
-    markdown-mode
-
-    ;; puppet
-    puppet-mode
-
-    ;; python
-    elpy
-    nose
-    py-autopep8
-    pyvenv
-
-    ;; yaml
-    yaml-mode))
-
-;; install the packages, dependent on the Emacs version
+;; install use-package
 (require 'mjb-install-packages)
+
+(eval-when-compile
+  (require 'use-package))
+
+(use-package ag
+  :ensure t)
+(use-package counsel
+  :ensure t)
+(use-package csv-mode
+  :ensure t)
+(use-package dash
+  :ensure t)
+(use-package ein
+  :ensure t)
+(use-package expand-region
+  :ensure t)
+(use-package hide-comnt
+  :ensure t)
+(use-package hydra
+  :ensure t)
+(use-package ivy
+  :ensure t)
+(use-package ivy-hydra
+  :ensure t)
+(use-package magit
+  :ensure t)
+(use-package multiple-cursors
+  :ensure t)
+(use-package phi-search
+  :ensure t)
+(use-package projectile
+  :ensure t)
+(use-package rainbow-delimiters
+  :ensure t)
+(use-package smex
+  :ensure t)
+(use-package swiper
+  :ensure t)
+(use-package wc-mode
+  :ensure t)
+(use-package wgrep
+  :ensure t)
+(use-package wgrep-ag
+  :ensure t)
+(use-package zenburn-theme
+  :ensure t)
+(use-package dockerfile-mode
+  :ensure t)
+(use-package feature-mode
+  :ensure t)
+(use-package js2-mode
+  :ensure t)
+(use-package json-mode
+  :ensure t)
+(use-package json-reformat
+  :ensure t)
+(use-package json-snatcher
+  :ensure t)
+(use-package markdown-mode
+  :ensure t)
+(use-package puppet-mode
+  :ensure t)
+(use-package elpy
+  :ensure t)
+(use-package nose
+  :ensure t)
+(use-package py-autopep8
+  :ensure t)
+(use-package pyvenv
+  :ensure t)
+(use-package yaml-mode
+  :ensure t)
 
 (provide 'mjb-packages)
