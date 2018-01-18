ee9ed391a3ac51001bef786f5855a4d2c879b716
Michael Brandt <michaelbrandt5@gmail.com>
2018-01-18 14:42:32 -0700

    tide-mode for tsx

--- settings/mjb-packages.el
+++ settings/mjb-packages.el
@@ -213,6 +213,16 @@
 ;; typescript
 (use-package tide)
 
+(use-package web-mode
+  :config
+  (add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
+  (add-hook 'web-mode-hook
+            (lambda ()
+              (when (string-equal "tsx" (file-name-extension buffer-file-name))
+                (setup-tide-mode))))
+  ;; enable typescript-tslint checker
+  (flycheck-add-mode 'typescript-tslint 'web-mode))
+
 ;; yaml
 (use-package yaml-mode)
 
