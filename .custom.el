28e4040bdb0576f3a1e04d1f8ec076fc0545a84d
Michael Brandt <michaelbrandt5@gmail.com>
2018-08-01 09:40:24 -0600

    .custom.el

--- .custom.el
+++ .custom.el
@@ -16,7 +16,10 @@
     (magit-insert-status-headers magit-insert-merge-log magit-insert-rebase-sequence magit-insert-am-sequence magit-insert-sequencer-sequence magit-insert-bisect-output magit-insert-bisect-rest magit-insert-bisect-log magit-insert-untracked-files magit-insert-unstaged-changes magit-insert-staged-changes magit-insert-stashes magit-insert-unpulled-from-upstream magit-insert-unpulled-from-pushremote magit-insert-unpushed-to-pushremote magit-insert-unpushed-to-upstream)))
  '(org-clock-persist-file "~/.emacs.d/.org-clock-save.el")
  '(safe-local-variable-values (quote ((encoding . utf-8))))
- '(uniquify-buffer-name-style (quote forward) nil (uniquify)))
+ '(typescript-indent-level 2)
+ '(uniquify-buffer-name-style (quote forward) nil (uniquify))
+ '(web-mode-code-indent-offset 2)
+ '(web-mode-markup-indent-offset 2))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
